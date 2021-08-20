7addc7fef9b9f6a59efa887141157abee7a5c9f6
Zoya Sanjeev <zoyasanjeev99@gmail.com>
2021-08-20 14:19:03 +0530

    USC1 employee present or absent

---
+++ src/com/bridgelabz/EmployeeWage/EmployeeWageCompute.java
@@ -0,0 +1,17 @@
+package com.bridgelabz.EmployeeWage;
+
+public class EmployeeWageCompute {
+
+	public static void main(String[] args) {
+		int IS_FULL_TIME=1;
+		double empCheck= Math.floor(Math.random()*10 %2 );
+		if(empCheck==IS_FULL_TIME) {
+			System.out.println("Employee is present");
+		}
+		else {
+			System.out.println("Employee is absent");
+		}
+
+	}
+
+}

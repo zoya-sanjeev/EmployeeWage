69991603ac5f7e3edb33464dfc30478b677480a2
Zoya Sanjeev <zoyasanjeev99@gmail.com>
2021-08-20 15:18:10 +0530

    UC3 Calculating part time wages

--- src/com/bridgelabz/EmployeeWage/EmployeeWageCompute.java
+++ src/com/bridgelabz/EmployeeWage/EmployeeWageCompute.java
@@ -1,15 +1,23 @@
 package com.bridgelabz.EmployeeWage;
 
 public class EmployeeWageCompute {
-	public static final int employeePresent = 1;
-	public static final int fullTime = 8;
+	
+	public static final int absent = 0;
+	public static final int fullTime = 2;
+	public static final int partTime = 1;
+	public static final int fullTimeHrs = 8;
 	public static final int wagePerHour = 20;
 	public static void main(String[] args) {
 		double empCheck= Math.floor(Math.random()*10 %2 );
 		int dailyWage = 0,hours=0;
-		if(empCheck == employeePresent) {
-			hours = fullTime;
+		if(empCheck == fullTime) {
+			hours = fullTimeHrs;
 		}
+		else if(empCheck==partTime){
+			hours= fullTimeHrs/2;
+		}
+		else
+			hours=0;
 		System.out.println("Daily Wage = "+(hours*wagePerHour));
 	}
 

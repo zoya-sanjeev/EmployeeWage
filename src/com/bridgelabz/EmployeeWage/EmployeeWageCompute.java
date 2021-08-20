9138119f3f0da3d4da4bb03a3cff4eafdb32b54f
Zoya Sanjeev <zoyasanjeev99@gmail.com>
2021-08-20 15:49:44 +0530

    Working hours condition

--- src/com/bridgelabz/EmployeeWage/EmployeeWageCompute.java
+++ src/com/bridgelabz/EmployeeWage/EmployeeWageCompute.java
@@ -8,9 +8,9 @@ public class EmployeeWageCompute {
 	public static final int fullTimeHrs = 8;
 	public static final int wagePerHour = 20;
 	public static void main(String[] args) {
-		int totalHrs = 0;
-		int noOfDays = 20;
-		for(int i=0;i<noOfDays;i++) {
+		int totalHrs = 0,i=0;
+		int noOfDays = 20, maxHrs = 100;		
+		while(totalHrs < 100 && i < noOfDays) {
 			int hours=0;
 			double empCheck = Math.floor(Math.random()*10)%3;
 			switch((int)empCheck) {
@@ -23,8 +23,11 @@ public class EmployeeWageCompute {
 			case absent: hours = 0;
 			}
 			totalHrs += hours;
+			i++;
+			System.out.println("Day "+i+": "+hours+" hours");
 		}
-		System.out.println("Monthly wage= "+(totalHrs*wagePerHour));
+		totalHrs = totalHrs>100 ? 100 : totalHrs;
+		System.out.println(i+" "+totalHrs+" Monthly Wage = "+(totalHrs*wagePerHour));
 	
 	}
 

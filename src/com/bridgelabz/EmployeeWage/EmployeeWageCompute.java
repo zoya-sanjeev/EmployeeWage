dc2a6d234f32ebfa7e36233f5b9f4c89f4c1fd94
Zoya Sanjeev <zoyasanjeev99@gmail.com>
2021-08-27 15:37:00 +0530

    REFACTORED:changed constants and used proper naming convention

--- src/com/bridgelabz/EmployeeWage/EmployeeWageCompute.java
+++ src/com/bridgelabz/EmployeeWage/EmployeeWageCompute.java
@@ -2,32 +2,33 @@ package com.bridgelabz.EmployeeWage;
 
 public class EmployeeWageCompute {
 	
-	public static final int ABSENT = 0;
 	public static final int FULLTIME = 2;
 	public static final int PARTTIME = 1;
 	public static final int FULL_TIME_HOURS = 8;
 	
-	public static void companyWageCompute(String companyName,int wagePerHour, int noOfDays, int maxHours) {
+	public static void companyWageCompute(String companyName,int wagePerHour, int noOfDays, int maxHoursPerMonth) {
 		
-		int day=0, totalHours=0;
-		while(totalHours < 100 && day < noOfDays) {
-			int hours=0;
+		int workingDays=0, totalEmpHours=0;
+		int employeeHours=0;
+		while(totalEmpHours <=maxHoursPerMonth && workingDays < noOfDays) {
+			
 			double empCheck = Math.floor(Math.random()*10)%3;
 			switch((int)empCheck) {
-			case FULLTIME: hours = FULL_TIME_HOURS;
+			case FULLTIME: employeeHours = FULL_TIME_HOURS;
 			break;
 			
-			case PARTTIME: hours = FULL_TIME_HOURS/2;
+			case PARTTIME: employeeHours = FULL_TIME_HOURS/2;
 			break;
 			
-			case ABSENT: hours = 0;
+			default: employeeHours = 0;
 			}
-			totalHours += hours;
-			day++;
-			System.out.println("Day "+day+": "+hours+" hours");
+			totalEmpHours += employeeHours;
+			workingDays++;
+			System.out.println("Day "+workingDays+": "+employeeHours+" hours");
 		}
-		totalHours = totalHours>100 ? 100 : totalHours;
-		System.out.println(companyName+":Days:"+day+" Total Hours:"+totalHours+" Monthly Wage = "+(totalHours*wagePerHour));
+		int totalEmpWage=totalEmpHours*wagePerHour;
+		
+		System.out.println("Total Employee Wage for Company "+companyName+" is "+totalEmpWage);
 		
 	}
 	public static void main(String[] args) {
